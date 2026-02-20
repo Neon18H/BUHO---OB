bfa3f090040ee4d83abf6ef3461f829415e32335
Fabio Hams Valois <92942417+Neon18H@users.noreply.github.com>
2026-02-19 22:56:50 -0500

    Upgrade NOC auth UX and fix clearance navigation

--- buho/urls.py
+++ buho/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import include, path
 
 from agents.views import ThreatsOverviewView
-from ui.views import BuhoLogoutView
+from ui.views import BuhoLoginView as LoginView, BuhoLogoutView, RegisterView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/login/', LoginView.as_view(), name='auth_login'),
+    path('auth/register/', RegisterView.as_view(), name='auth_register'),
     path('', include(('ui.urls', 'ui'), namespace='ui')),
     path('', include(('accounts.urls', 'accounts'), namespace='accounts')),
     path('agents/', include(('agents.urls', 'agents'), namespace='agents')),
